d88b7628cac1eabeee335487b1fe09aeb6f015cd
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2024-01-04 16:47:23 -0600

    Update3 client.py

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ def make_prediction(inputs):
         return None
 
 def display_predictions(prediction):
-    st.write(f"\nEl número mayor es: {prediction}")
+    st.write(f"\nEl número mayor es: {prediction['outputs'][0]['dense_4']}")
 
 def main():
     st.title('Predictor del Número Mayor')
@@ -41,30 +41,8 @@ def main():
         prediction = make_prediction(inputs)
 
         if prediction:
-            display_predictions(prediction['predictions'][0])
+            display_predictions(prediction)
 
 if __name__ == '__main__':
     main()
 
-    num2 = st.text_input('Número 2', value='0.0')
-    num3 = st.text_input('Número 3', value='0.0')
-
-    if st.button('Predecir'):
-        try:
-            num1 = float(num1)
-            num2 = float(num2)
-            num3 = float(num3)
-        except ValueError:
-            st.error("Por favor, ingresa números válidos.")
-            return
-
-        inputs = [
-            [num1, num2, num3]
-        ]
-        predictions = make_prediction(inputs)
-
-        if predictions:
-            display_predictions(predictions['predictions'][0], "Resultados")
-
-if __name__ == '__main__':
-    main()
