a2abdae43d3dd382312654d8d2f76a05f70d1631
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2024-01-04 16:52:13 -0600

    Update 4client.py

--- client.py
+++ client.py
@@ -9,13 +9,17 @@ def make_prediction(inputs):
     
     if response.status_code == 200:
         prediction = response.json()
+        st.write(f"Respuesta JSON del modelo: {prediction}")
         return prediction
     else:
         st.error("Error al obtener predicciones. Por favor, verifica tus datos e intenta nuevamente.")
         return None
 
 def display_predictions(prediction):
-    st.write(f"\nEl número mayor es: {prediction['outputs'][0]['dense_4']}")
+    # Actualiza esta parte en función de la estructura específica de la respuesta del modelo
+    # Asumiendo que la estructura es algo como prediction['outputs'][0]['nombre_de_la_capa']
+    result = prediction['outputs'][0]['nombre_de_la_capa']
+    st.write(f"\nEl número mayor es: {result}")
 
 def main():
     st.title('Predictor del Número Mayor')
